702e8bb57d784d893b6d1f4bb848acacc3067157
@yahya.FutureCode <yahya@futurecode-co.com>
2023-09-25 05:50:08 -0700

    first

--- routes/Count.js
+++ routes/Count.js
@@ -7,74 +7,99 @@ const { verifyAdmin } = require("../utils/TokenVerify");
 const router = express.Router()
 
 
-router.get("/" , verifyAdmin , async (req, res) => {
-    try {
-      const today = new Date();
-      const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-      const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-  
-      const countClient = await clientModel.countDocuments({
-        createdAt: {
-          $gte: firstDayOfMonth,
-          $lte: lastDayOfMonth,
-        },
-      });
-      const countProduct = await productModel.countDocuments({
-        createdAt: {
-          $gte: firstDayOfMonth,
-          $lte: lastDayOfMonth,
-        },
-      });
-      const countOrder = await orderModel.countDocuments({
-        createdAt: {
-          $gte: firstDayOfMonth,
-          $lte: lastDayOfMonth,
-        },
-      });
-  
-      res.status(200).send({ order : countOrder , product : countProduct , client  :countClient });
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  }
-)
-
-router.get("/getYear" , verifyAdmin , async (req, res) => {
+router.get("/", verifyAdmin, async (req, res) => {
   try {
-    const orders =await orderModel.find();
-    const products = await productModel.find();
+    const today = new Date();
+    const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
 
-    const months = Array.from({ length: 12 }, (_, i) => i + 1);
+    const orders = await orderModel.find({
+      createdAt: {
+        $gte: firstDayOfMonth,
+        $lte: lastDayOfMonth,
+      },
+    });
 
-    const countsByMonth = months.map((month) => {
-      const ordersInMonth = orders.filter((order) => {
-        const orderDate = new Date(order.createdAt);
-        return orderDate.getMonth() + 1 === month;
-      });
-      return {
-        month,
-        count: ordersInMonth.length,
-      };
-    })
-    
-    const countsByMonthProduct = months.map((month) => {
-      const productsInMonth = products.filter((product) => {
-        const productDate = new Date(product.createdAt);
-        return productDate.getMonth() + 1 === month;
-      });
+    let totalProfit = 0;
+    for (const order of orders) {
+      const orderProfit = order.qty * order.price;
+      totalProfit += orderProfit;
+    }
 
-      return {
-        month,
-        count: productsInMonth.length,
-      };
-    })
+    const countClient = await clientModel.countDocuments({
+      createdAt: {
+        $gte: firstDayOfMonth,
+        $lte: lastDayOfMonth,
+      },
+    });
+    const countProduct = await productModel.countDocuments({
+      createdAt: {
+        $gte: firstDayOfMonth,
+        $lte: lastDayOfMonth,
+      },
+    });
+    const countOrder = orders.length;
 
-    res.status(200).json({ order :countsByMonth , product :countsByMonthProduct });
+    res.status(200).send({ order: countOrder, product: countProduct, client: countClient, revenue : totalProfit });
   } catch (error) {
     res.status(500).send(error);
   }
-}
-)
+});
+
+router.get("/getYear", verifyAdmin, async (req, res) => {
+  try {
+    const today = new Date();
+    const currentYear = today.getFullYear();
+
+    const months = Array.from({ length: 12 }, (_, i) => i + 1);
+
+    const countsByMonth = await Promise.all(
+      months.map(async (month) => {
+        const firstDayOfMonth = new Date(currentYear, month - 1, 1);
+        const lastDayOfMonth = new Date(currentYear, month, 0);
+
+        const ordersInMonth = await orderModel.find({
+          createdAt: {
+            $gte: firstDayOfMonth,
+            $lte: lastDayOfMonth,
+          },
+        });
+
+        return {
+          month,
+          count: ordersInMonth.length,
+        };
+      })
+    );
+    const countsByMonthPrice = await Promise.all(
+      months.map(async (month) => {
+        const firstDayOfMonth = new Date(currentYear, month - 1, 1);
+        const lastDayOfMonth = new Date(currentYear, month, 0);
+
+        const ordersInMonth = await orderModel.find({
+          createdAt: {
+            $gte: firstDayOfMonth,
+            $lte: lastDayOfMonth,
+          },
+        });
+
+        let profitInMonth = 0;
+        for (const order of ordersInMonth) {
+          const orderProfit = order.qty * order.price;
+          profitInMonth += orderProfit;
+        }
+        return {
+          month,
+          count: profitInMonth,
+        };
+      })
+    );
+
+    res.status(200).json({ order: countsByMonth , revenue: countsByMonthPrice });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 
 module.exports = router
